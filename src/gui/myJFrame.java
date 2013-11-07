cb88c02e7d4304423505e35d0c5edb93d3b1424b
kspytkowski <kspytkowski@wp.pl>
2013-11-07 20:28:32 +0100

    nice looking GUI

--- src/gui/myJFrame.java
+++ src/gui/myJFrame.java
@@ -20,6 +20,14 @@ import javax.swing.UIManager;
 import javax.swing.border.TitledBorder;
 import javax.swing.JMenu;
 import javax.swing.JTextField;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import com.jgoodies.forms.layout.RowSpec;
+import java.awt.FlowLayout;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class myJFrame extends JFrame {
 
@@ -47,63 +55,78 @@ public class myJFrame extends JFrame {
 	 */
 	public myJFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 717, 470);
+		setBounds(100, 100, 1052, 470);
 		contentPane = new JPanel();
 		contentPane.setBorder(UIManager.getBorder("ComboBox.border"));
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
 		
 		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "New crossword", TitledBorder.LEFT, TitledBorder.TOP, null, UIManager.getColor("TitledBorder.titleColor")));
-		panel.setAlignmentX(Component.RIGHT_ALIGNMENT);
-		panel.setBounds(10, 11, 364, 53);
+		panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "New crossword", TitledBorder.LEFT, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		//panel.setAlignmentX(Component.RIGHT_ALIGNMENT);
+		panel.setBounds(10, 11, 328, 71);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Height");
-		lblNewLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-		lblNewLabel.setPreferredSize(new Dimension(100, 40));
-		lblNewLabel.setMinimumSize(new Dimension(100, 40));
-		lblNewLabel.setMaximumSize(new Dimension(100, 40));
-		lblNewLabel.setBounds(10, 21, 44, 20);
-		panel.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("Width");
-		lblNewLabel_1.setAlignmentX(Component.CENTER_ALIGNMENT);
-		lblNewLabel_1.setBounds(142, 24, 35, 14);
-		panel.add(lblNewLabel_1);
+		JLabel lblHeight = new JLabel("Height");
+		lblHeight.setBounds(20, 27, 37, 20);
+		panel.add(lblHeight);
 		
 		JSpinner spinner = new JSpinner();
-		spinner.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		spinner.setBounds(71, 21, 29, 20);
+		spinner.setBounds(67, 27, 34, 21);
 		panel.add(spinner);
 		
+		JLabel lblWidth = new JLabel("Width");
+		lblWidth.setBounds(121, 27, 34, 20);
+		panel.add(lblWidth);
+		
 		JSpinner spinner_1 = new JSpinner();
-		spinner_1.setBounds(193, 21, 29, 20);
+		spinner_1.setBounds(163, 27, 34, 21);
 		panel.add(spinner_1);
 		
 		JButton btnGenerate = new JButton("Generate");
-		btnGenerate.setBounds(248, 20, 89, 23);
+		btnGenerate.setBounds(219, 26, 89, 23);
 		panel.add(btnGenerate);
+		
+		
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "From file", TitledBorder.LEFT, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		panel_1.setBounds(398, 11, 280, 53);
+		panel_1.setBounds(348, 11, 339, 71);
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
 		
 		textField = new JTextField("Path...");
-		textField.setBounds(10, 22, 86, 20);
+		textField.setBounds(20, 27, 146, 21);
 		panel_1.add(textField);
 		textField.setColumns(10);
 		
 		JButton btnNewButton = new JButton("...");
-		btnNewButton.setBounds(106, 21, 39, 23);
+		btnNewButton.setBounds(171, 26, 39, 23);
 		panel_1.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Load");
-		btnNewButton_1.setBounds(168, 19, 89, 23);
+		btnNewButton_1.setBounds(230, 26, 89, 23);
 		panel_1.add(btnNewButton_1);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Control", TitledBorder.LEFT, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_2.setBounds(697, 11, 329, 71);
+		contentPane.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JButton btnNewButton_2 = new JButton("Solve");
+		btnNewButton_2.setBounds(20, 26, 89, 23);
+		panel_2.add(btnNewButton_2);
+		
+		JButton btnSave = new JButton("Save");
+		btnSave.setBounds(119, 26, 89, 23);
+		panel_2.add(btnSave);
+		
+		JButton btnPrint = new JButton("Print");
+		btnPrint.setBounds(218, 26, 89, 23);
+		panel_2.add(btnPrint);
+		
 		
 		JFileChooser pliki = new JFileChooser(".");
 		if (JFileChooser.APPROVE_OPTION==pliki.showOpenDialog(this))
