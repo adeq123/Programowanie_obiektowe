ac4a7e11f2f070a54786e324d2cb99339d5bf9d5
kspytkowski <kspytkowski@wp.pl>
2013-11-23 16:06:53 +0100

    lab7 changed

--- src/Lab7/DB.java
+++ src/Lab7/DB.java
@@ -17,7 +17,8 @@ public class DB {
 		try {
 			Class.forName("org.postgresql.Driver").newInstance();
 			conn = DriverManager.getConnection(
-					"jdbc:mysql://mysql.agh.edu.pl/books", "kspyt", "moje_haslo");
+					"jdbc:postgresql://mysql.agh.edu.pl/books",
+					"kspyt", "");
 
 		} catch (SQLException ex) {
 			// handle any errors
@@ -162,7 +163,7 @@ public class DB {
 			stmt = conn.createStatement();
 			System.out.print("Podaj klucz (ISBN):");
 			String isbn = JIn.getString();
-			
+
 			rs = stmt.executeQuery("SELECT * FROM books WHERE isbn =" + isbn);
 
 			while (rs.next()) {
@@ -196,7 +197,7 @@ public class DB {
 			stmt = conn.createStatement();
 			System.out.print("Podaj klucz (autora):");
 			String author = JIn.getString();
-			
+
 			stmt.executeUpdate("DELETE FROM books WHERE author = '" + author
 					+ "'");
 		} catch (SQLException ex) {
@@ -219,15 +220,14 @@ public class DB {
 			}
 		}
 	}
-	
+
 	public void deleteByISBN() {
 		try {
 			stmt = conn.createStatement();
 			System.out.print("Podaj klucz (ISBN):");
 			String isbn = JIn.getString();
-			
-			stmt.executeUpdate("DELETE FROM books WHERE isbn = '" + isbn
-					+ "'");
+
+			stmt.executeUpdate("DELETE FROM books WHERE isbn = '" + isbn + "'");
 		} catch (SQLException ex) {
 			// handle any errors
 		} finally {
@@ -248,39 +248,40 @@ public class DB {
 			}
 		}
 	}
-	
-	public void printDB(){
-	    try {
-	      connect();
-	      stmt = conn.createStatement();
-	      rs = stmt.executeQuery("SELECT * FROM books");
-	      while(rs.next()){
-	        String record = rs.getString(1)+ " | "+rs.getString(2)+ " | "+rs.getString(3)+ " | "+rs.getString(4);
-	        System.out.println(record);
-	      }
-	    }catch (SQLException ex){
-	      // handle any errors
-	    }finally {
-	      // zwalniamy zasoby, które nie będą potrzebne
-	      if (rs != null) {
-	        try {
-	          rs.close();
-	        } catch (SQLException sqlEx) { } // ignore
-	        rs = null;
-	      }
-	      if (stmt != null) {
-	        try {
-	          stmt.close();
-	        } catch (SQLException sqlEx) { } // ignore
-	        stmt = null;
-	      }
-	    }
-	  }
 
-	
-	
+	public void printDB() {
+		try {
+			connect();
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("SELECT * FROM books");
+			while (rs.next()) {
+				String record = rs.getString(1) + " | " + rs.getString(2)
+						+ " | " + rs.getString(3) + " | " + rs.getString(4);
+				System.out.println(record);
+			}
+		} catch (SQLException ex) {
+			// handle any errors
+		} finally {
+			// zwalniamy zasoby, które nie będą potrzebne
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException sqlEx) {
+				} // ignore
+				rs = null;
+			}
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException sqlEx) {
+				} // ignore
+				stmt = null;
+			}
+		}
+	}
+
 	public static void main(String[] argv) {
 		DB database = new DB();
-		database.connect();
+		database.printDB();
 	}
 }
